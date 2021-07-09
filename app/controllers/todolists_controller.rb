deeb2e0fd7a332e26d0413b9047aa5b6f5080771
yuuki1771 <yuuki17711015@gmail.com>
2021-07-09 13:50:15 +0000

    dmm webcamp2

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,28 @@
+class TodolistsController < ApplicationController
+  def new
+     @list = List.new
+  end
+  
+  def create
+    # １. データを新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # ２. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # ３. トップ画面へリダイレクト
+    redirect_to todolist_path(list.id)
+  end
+  
+  def index
+     @lists = List.all
+  end
+  
+  def show
+    @list = List.find(params[:id])
+  end
+
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+end
